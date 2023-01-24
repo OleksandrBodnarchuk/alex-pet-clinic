84d61d5d40e21d4e58b4d8f6ff6060f707838550
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-01-24 16:27:16 +0100

    Initial commit

---
+++ src/test/java/pl/alex/petclinic/AlexPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.alex.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
