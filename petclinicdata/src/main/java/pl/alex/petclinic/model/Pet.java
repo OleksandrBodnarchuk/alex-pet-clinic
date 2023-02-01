1c2fffe4f110f93e1a543af26d34a21ef79ca995
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-01 16:37:22 +0100

    Implemented Map Based Services. Closes #6

--- petclinicdata/src/main/java/pl/alex/petclinic/model/Pet.java
+++ petclinicdata/src/main/java/pl/alex/petclinic/model/Pet.java
@@ -4,9 +4,18 @@ import java.time.LocalDate;
 
 public class Pet {
 	
+	private Long id;
 	private LocalDate birthDate;
 	private PetType petType;
 	private Owner owner;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public LocalDate getBirthDate() {
 		return birthDate;
