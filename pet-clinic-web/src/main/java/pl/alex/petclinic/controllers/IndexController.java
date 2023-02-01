e3d0ff03c80ab21e9d693539cd1b489223d4dd14
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-01 17:12:40 +0100

    Added index page controller. Closes #8

---
+++ pet-clinic-web/src/main/java/pl/alex/petclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package pl.alex.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET, value = { "", "/", "index", "index.html" })
+	public String index() {
+		return "index";
+	}
+}
