969bf83eff116101a981e59b062c8da570b2f69f
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-01 16:19:52 +0100

    Refactored to common interface. Closes #11

--- petclinicdata/src/main/java/pl/alex/petclinic/service/PetService.java
+++ petclinicdata/src/main/java/pl/alex/petclinic/service/PetService.java
@@ -1,15 +1,7 @@
 package pl.alex.petclinic.service;
 
-import java.util.Set;
-
 import pl.alex.petclinic.model.Pet;
 
-public interface PetService {
-
-	Pet findById(Long id);
-
-	Pet save(Pet pet);
-
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
