5f8603c52afe9a4c4d5fafb7a3968e6a13c5277e
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-02 17:12:16 +0100

    Added list of Vets to the view. Closes #16

--- pet-clinic-web/src/main/java/pl/alex/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/alex/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package pl.alex.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.alex.petclinic.service.VetService;
 
 @Controller
 public class VetController {
 
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html" })
-	public String listVets() {
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
