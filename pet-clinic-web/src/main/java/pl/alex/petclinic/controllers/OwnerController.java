b19bb42e0b3b09539e175cbb628cfc35d7c183cd
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-02 17:09:19 +0100

    Added owners list. Closes #17

--- pet-clinic-web/src/main/java/pl/alex/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/alex/petclinic/controllers/OwnerController.java
@@ -1,14 +1,28 @@
 package pl.alex.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.alex.petclinic.model.Owner;
+import pl.alex.petclinic.service.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({ "", "/", "/index", "/index.html" })
-	public String listOwners() {
+	public String listOwners(Model model) {
+		Set<Owner> owners = ownerService.findAll();
+		model.addAttribute("owners", owners);
 		return "owners/index";
 	}
 }
