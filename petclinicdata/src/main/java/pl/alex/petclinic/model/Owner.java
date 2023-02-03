638c4e2fe9cdb8a600c3e084ffc0cee92473f75f
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-03 19:02:10 +0100

    Added Visit entity and associated Pet with Owner. Closes #27

--- petclinicdata/src/main/java/pl/alex/petclinic/model/Owner.java
+++ petclinicdata/src/main/java/pl/alex/petclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package pl.alex.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	private static final long serialVersionUID = 7333165978320769543L;
+
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }
