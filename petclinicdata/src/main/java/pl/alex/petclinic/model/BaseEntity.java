a0d3c75ba976e147666fca91e13988c15a95a5ad
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-04 19:18:33 +0100

    Implemented BaseEntity. Closes #36

--- petclinicdata/src/main/java/pl/alex/petclinic/model/BaseEntity.java
+++ petclinicdata/src/main/java/pl/alex/petclinic/model/BaseEntity.java
@@ -2,10 +2,18 @@ package pl.alex.petclinic.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	private static final long serialVersionUID = -5987401129673597710L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
