974b2eedf1da9bd0cbdc53340c3a635e76494b6a
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-04 20:15:21 +0100

    Created VetJpaService. Closes #42

---
+++ petclinicdata/src/main/java/pl/alex/petclinic/service/jpa/VetJpaService.java
@@ -0,0 +1,51 @@
+package pl.alex.petclinic.service.jpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import pl.alex.petclinic.model.Vet;
+import pl.alex.petclinic.repositories.VetRepository;
+import pl.alex.petclinic.service.VetService;
+
+@Service
+@Profile("springdatajpa")
+public class VetJpaService implements VetService {
+
+	private final VetRepository vetRepository;
+
+	public VetJpaService(VetRepository vetRepository) {
+		this.vetRepository = vetRepository;
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+		Set<Vet> vets = new HashSet<>();
+		vetRepository.findAll().forEach(vets::add);
+		return vets;
+
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return vetRepository.save(object);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		vetRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);
+	}
+
+}
